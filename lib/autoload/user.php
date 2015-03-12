5b84c38b597f74add373989ac1fbb446a7c427e2
Krupodorov Roman <krupod@gmail.com>
2015-03-12 15:29:52 +0200

    Syncing fixed.

---
+++ lib/autoload/user.php
@@ -0,0 +1,36 @@
+<?
+
+class User
+{
+	public $id;
+	public $data;
+
+	public function __construct($id = false)
+	{
+		if($id)
+		{
+			$this->id = $id;
+
+			$this->data = DB::query_row( "SELECT * FROM ebay_users WHERE user_id = $id");
+		}
+	}
+
+	public static function from_data($data)
+	{
+		$user = new User();
+
+		$user->id = $data['user_id'];
+
+		$user->data = $data;
+
+		return $user;
+	}
+
+	public function notify($subject, $message)
+	{
+		$email = $this->data['email'];
+
+		mail($email, $subject, $message);
+	}
+
+}
